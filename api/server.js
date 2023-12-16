94b4dace2c6f8cbd9bab9a2298e4dd43b7e6cbe9
Sergio González Sánchez <soysergiogonzalez@outlook.es>
2023-12-16 18:53:29 -0500

    feat: se sube actualización a la API

--- api/server.js
+++ api/server.js
@@ -6,13 +6,15 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/product/:resource/:id/show': '/:resource/:id'
-}))
+// server.use(jsonServer.rewriter({
+//     '/api/*': '/$1',
+//     '/product/:resource/:id/show': '/:resource/:id'
+// }))
+
+const PORT = process.env.PORT || 3000
 server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
+server.listen(PORT, () => {
+    console.log(`JSON Server is running on port ${PORT}`)
 })
 
 // Export the Server API
